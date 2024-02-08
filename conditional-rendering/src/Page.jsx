120e2f08acef78f4da633167632288499ed81f62
mooojn <moontariq7867@gmail.com>
2024-02-08 15:08:45 +0500

    used ternary condition instead of if elsa

--- conditional-rendering/src/Page.jsx
+++ conditional-rendering/src/Page.jsx
@@ -1,8 +1,5 @@
 
 function Page(props){
-    if(props.isLoggedIn){
-        return <h3>Welcome {props.userName}</h3>
-    }
-    return <h3>You need to log in first</h3>
+    return props.isLoggedIn ? <h3>Welcome {props.userName}</h3> : <h3>You need to log in first</h3>
 }
 export default Page;
